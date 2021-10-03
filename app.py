55c6c8d55d61df0eeeeccba5f8af6e6dee63c1a1
Mitaliii <66302933+Mitaliii@users.noreply.github.com>
2021-10-03 17:25:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,23 @@
-import numpy as np
-import pandas as pd
+from flask import Flask,render_template,request,url_for,jsonify,redirect
 import requests
-from flask import Flask, url_for, jsonify, render_template, request, redirect
+import pandas as pd
+import numpy as np
 import pickle
-model = pickle.load(open('heart_prediction.pkl', 'rb'))
-app = Flask(__name__)
 
-@app.route('/',methods=['POST','GET])
+model = pickle.load(open('heart_prediction.pkl','rb'))
+
+app=Flask(__name__)
+
+@app.route('/',methods=['GET'])
+def index():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
 def predict():
-    input= [[49.0,1,80,0,30,1,427000.0,1.0,138,0,0,12]]
-    input= pd.DataFrame(input)
-    output = model.predict(input)
-    return "Predicted Output" + str(output[0])  
+    jsson=request.form.to_dict()
+    values_list=list(jsson.values())
+    output=model.predict(pd.DataFrame([values_list]))
+    return render_template('predict.html',prediction_text="Heart Failure Prediction : "+str(output[0]))
 
-if __name__ == "__main__":
+if __name__=="__main__":
     app.run(debug=True,port=5000)
